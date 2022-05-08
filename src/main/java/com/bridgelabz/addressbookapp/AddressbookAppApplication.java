2a7575762e03ae649996fe329ed6df1ed39a2c1c
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-08 22:05:45 +0530

    Added: Welcome message and dependency for project

---
+++ src/main/java/com/bridgelabz/addressbookapp/AddressbookAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressbookAppApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to Addressbook App Application");
+		SpringApplication.run(AddressbookAppApplication.class, args);
+	}
+
+}
