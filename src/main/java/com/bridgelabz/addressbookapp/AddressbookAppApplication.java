ecaa374acbe32318ab2f8d6a08574434bbb2d6cd
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-11 00:51:02 +0530

    Added: Use Lombok Library for Logging

--- src/main/java/com/bridgelabz/addressbookapp/AddressbookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressbookAppApplication.java
@@ -1,14 +1,16 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookAppApplication {
 
 	public static void main(String[] args) {
         System.out.println("Welcome to Addressbook App Application");
 		SpringApplication.run(AddressbookAppApplication.class, args);
+		log.info("Addressbook App Started");
 	}
-
 }
