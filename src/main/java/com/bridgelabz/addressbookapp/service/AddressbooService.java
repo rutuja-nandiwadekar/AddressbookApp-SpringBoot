0c64b428e7caefe365b04f96a3e7e661bedbc1ad
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-10 23:52:01 +0530

    Added: Code to Introduce Services Layer in AddressBook App

---
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressbooService.java
@@ -0,0 +1,46 @@
+package com.bridgelabz.addressbookapp.service;
+
+import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
+import com.bridgelabz.addressbookapp.model.AddressbookData;
+
+import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
+import com.bridgelabz.addressbookapp.model.AddressbookData;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AddressbooService implements IAddressbookService {
+    @Override
+    public List<AddressbookData> getAddressbookData() {
+        List<AddressbookData> addressbookDataList = new ArrayList<>();
+        addressbookDataList.add(new AddressbookData(1,new AddressbookDTO("karthik","7411949218")));
+        return addressbookDataList;
+    }
+
+    @Override
+    public AddressbookData getAddressbookDataById(int personId) {
+        AddressbookData addressbookData =  null;
+        addressbookData = new AddressbookData(personId,new AddressbookDTO("karthik","12345678"));
+        return addressbookData;
+    }
+
+    @Override
+    public AddressbookData createAddressbooData(AddressbookDTO addressbookDTO) {
+        AddressbookData addressbookData = null;
+        addressbookData = new AddressbookData(1,addressbookDTO);
+        return addressbookData;
+    }
+
+    @Override
+    public AddressbookData updateAddressbookData(int personId,AddressbookDTO addressbookDTO) {
+        AddressbookData addressbookData = null;
+        addressbookData = new AddressbookData(personId,addressbookDTO);
+        return addressbookData;
+    }
+
+    @Override
+    public void deleteAddressbooData(int personId) {
+    }
+}
---
+++ src/main/java/com/bridgelabz/addressbookapp/service/IAddressbookService.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.addressbookapp.service;
+
+import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
+import com.bridgelabz.addressbookapp.model.AddressbookData;
+
+import java.util.List;
+
+public interface IAddressbookService {
+    List<AddressbookData> getAddressbookData();
+
+    AddressbookData getAddressbookDataById(int personId);
+
+    AddressbookData createAddressbooData(AddressbookDTO addressbookDTO);
+
+    AddressbookData updateAddressbookData(int personId,AddressbookDTO addressbookDTO);
+
+    void deleteAddressbooData(int personId);
+
+}
