c42fc2ab743b66be6e398083fcf9af19dab75573
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-11 00:29:38 +0530

    Added: Code to store the Address Book Data

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressbookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressbookController.java
@@ -3,33 +3,41 @@ package com.bridgelabz.addressbookapp.controller;
 import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
 import com.bridgelabz.addressbookapp.dto.ResponseDTO;
 import com.bridgelabz.addressbookapp.model.AddressbookData;
+import com.bridgelabz.addressbookapp.service.IAddressbookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/addressbook")
 public class AddressbookController {
+
+    @Autowired
+    private IAddressbookService addressbookService;
+
     @RequestMapping(value = {"", "/", "/get"})
     public ResponseEntity<ResponseDTO> getAddressbookData() {
-        AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(1,new AddressbookDTO("Rutuja","7345679302"));
-        ResponseDTO responseDTO = new ResponseDTO("Get call success",addressbookData);
-        return new ResponseEntity<>(responseDTO,HttpStatus.OK);
+        List<AddressbookData> addressbookDataList = null;
+        addressbookDataList = addressbookService.getAddressbookData();
+        ResponseDTO respDTO = new ResponseDTO("Get Call Successful", addressbookDataList);
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 
     @GetMapping("/get/{personId}")
     public ResponseEntity<ResponseDTO> getAddressbookData(@PathVariable(value = "personId") int personId) {
         AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(personId,new AddressbookDTO("Rutwij","7411949218"));
-        ResponseDTO responseDTO = new ResponseDTO("Get Call Success for id: ",addressbookData);
+        addressbookData = addressbookService.getAddressbookDataById(personId);
+        ResponseDTO responseDTO = new ResponseDTO("Get Call Success for id: " +personId ,addressbookData);
         return new ResponseEntity<>(responseDTO,HttpStatus.OK);
     }
 
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> addAddressbookData(@RequestBody AddressbookDTO addressbookDTO) {
         AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(1,addressbookDTO);
+        addressbookData = addressbookService.createAddressbooData(addressbookDTO);
         ResponseDTO responseDTO = new ResponseDTO("Created Employee Payroll Data for: ",addressbookData);
         return new ResponseEntity<>(responseDTO,HttpStatus.OK);
     }
@@ -44,7 +52,8 @@ public class AddressbookController {
 
     @DeleteMapping("/delete/{personId}")
     public ResponseEntity<ResponseDTO> deleteAddressbookData(@PathVariable("personId") int personId) {
-        ResponseDTO responseDTO = new ResponseDTO("Deleted data successfully","person id: "+personId);
+        addressbookService.deleteAddressbooData(personId);
+        ResponseDTO responseDTO = new ResponseDTO("Deleted successfully","Deleted id: "+personId);
         return new ResponseEntity<>(responseDTO,HttpStatus.OK);
     }
   }
--- src/main/java/com/bridgelabz/addressbookapp/service/AddressbookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressbookService.java
@@ -2,45 +2,47 @@ package com.bridgelabz.addressbookapp.service;
 
 import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
 import com.bridgelabz.addressbookapp.model.AddressbookData;
-
-import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
-import com.bridgelabz.addressbookapp.model.AddressbookData;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class AddressbooService implements IAddressbookService {
+public class AddressbookService implements IAddressbookService {
+
+    List<AddressbookData> addressbookDataList = new ArrayList<>();
+
     @Override
     public List<AddressbookData> getAddressbookData() {
-        List<AddressbookData> addressbookDataList = new ArrayList<>();
-        addressbookDataList.add(new AddressbookData(1,new AddressbookDTO("karthik","7411949218")));
         return addressbookDataList;
     }
 
     @Override
     public AddressbookData getAddressbookDataById(int personId) {
-        AddressbookData addressbookData =  null;
-        addressbookData = new AddressbookData(personId,new AddressbookDTO("karthik","12345678"));
+        AddressbookData addressbookData = null;
+        addressbookData = addressbookDataList.get(personId - 1);
         return addressbookData;
     }
 
     @Override
     public AddressbookData createAddressbooData(AddressbookDTO addressbookDTO) {
         AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(1,addressbookDTO);
+        addressbookData = new AddressbookData(addressbookDataList.size() + 1, addressbookDTO);
+        addressbookDataList.add(addressbookData);
         return addressbookData;
     }
 
     @Override
-    public AddressbookData updateAddressbookData(int personId,AddressbookDTO addressbookDTO) {
-        AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(personId,addressbookDTO);
+    public AddressbookData updateAddressbookData(int personId, AddressbookDTO addressbookDTO) {
+        AddressbookData addressbookData = this.getAddressbookDataById(personId);
+        addressbookData.setName(addressbookDTO.name);
+        addressbookData.setPhoneNumber(addressbookDTO.phoneNumber);
+        addressbookDataList.set(personId - 1, addressbookData);
         return addressbookData;
     }
 
     @Override
     public void deleteAddressbooData(int personId) {
+        addressbookDataList.remove(personId -1);
     }
 }
