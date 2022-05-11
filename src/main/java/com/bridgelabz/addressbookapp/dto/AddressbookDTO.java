7a3c057e25c3b4fc1756307c85387fe031dafe0c
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-11 10:53:22 +0530

    Added: Code to add remaining properties to the Addressbook DTO and Model

--- src/main/java/com/bridgelabz/addressbookapp/dto/AddressbookDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressbookDTO.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.addressbookapp.dto;
 
-import lombok.Data;
+import lombok.ToString;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-@Data
-public class AddressbookDTO {
+
+public  @ToString class AddressbookDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Addressbook name is Invalid")
     @NotEmpty(message = "Addressbook name cannot be null")
@@ -15,16 +15,14 @@ public class AddressbookDTO {
     @Pattern(regexp = "^[91]+[ ]?[6-9]{1}[0-9]{9}$" ,message = "phonenumber is invalid")
     public String phoneNumber;
 
-    public AddressbookDTO(String name, String phoneNumber) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
+    public String address;
 
-    @Override
-    public String toString() {
-        return "AddressbookDTO{" +
-                "name='" + name + '\'' +
-                ", phoneNumber=" + phoneNumber +
-                '}';
-    }
+    public String city;
+
+    public String state;
+
+    public String zip;
+
+    public String email;
+
 }
--- src/main/java/com/bridgelabz/addressbookapp/model/AddressbookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressbookData.java
@@ -1,40 +1,28 @@
 package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
+import lombok.Data;
 
-public class AddressbookData {
+public  @Data class AddressbookData {
 
     private int personId;
     private String name;
     private String phoneNumber;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String email;
+
 
     public AddressbookData(int personId, AddressbookDTO addressbookDTO) {
         this.personId = personId;
         this.name = addressbookDTO.name;
         this.phoneNumber = addressbookDTO.phoneNumber;
-    }
-
-    public int getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(int personId) {
-        this.personId = personId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.address = addressbookDTO.address;
+        this.city = addressbookDTO.city;
+        this.state = addressbookDTO.state;
+        this.zip = addressbookDTO.zip;
+        this.email = addressbookDTO.email;
     }
 }
